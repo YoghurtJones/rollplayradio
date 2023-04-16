13d39b265a058a6fff1f430de4666e2586b1a39e
YoghurtJones <130816596+YoghurtJones@users.noreply.github.com>
2023-04-17 08:03:10 +1000

    Linked audio

--- audioplayer.js
+++ audioplayer.js
@@ -17,62 +17,62 @@ cardDivs.forEach(function(cardDiv, index) {
   });
 });
 
-// Update boxshadow to reflect music type
+// // Update boxshadow to reflect music type
 
-var cardBoxShadow = document.querySelectorAll(".music");
+// var cardBoxShadow = document.querySelectorAll(".music");
 
-cardBoxShadow.forEach(function(div) {
-  div.addEventListener("click", function() {
-    if (div.style.boxShadow) {
-      div.style.boxShadow = "";
-    } else {
-      div.style.boxShadow = "0 0 20px red";
-      console.log("Boxshadow style for music added to card.");
-    }
-  });
-});
+// cardBoxShadow.forEach(function(div) {
+//   div.addEventListener("click", function() {
+//     if (div.style.boxShadow) {
+//       div.style.boxShadow = "";
+//     } else {
+//       div.style.boxShadow = "0 0 20px red";
+//       console.log("Boxshadow style for music added to card.");
+//     }
+//   });
+// });
 
-var cardBoxShadow = document.querySelectorAll(".ambience");
+// var cardBoxShadow = document.querySelectorAll(".ambience");
 
-cardBoxShadow.forEach(function(div) {
-  div.addEventListener("click", function() {
-    if (div.style.boxShadow) {
-      div.style.boxShadow = "";
-    } else {
-      div.style.boxShadow = "0 0 20px greenyellow";
-      console.log("Boxshadow style for ambience added to card.");
-    }
-  });
-});
+// cardBoxShadow.forEach(function(div) {
+//   div.addEventListener("click", function() {
+//     if (div.style.boxShadow) {
+//       div.style.boxShadow = "";
+//     } else {
+//       div.style.boxShadow = "0 0 20px greenyellow";
+//       console.log("Boxshadow style for ambience added to card.");
+//     }
+//   });
+// });
 
-// GLOBAL PAUSE
+// // GLOBAL PAUSE
 
-// Get the existing div element
-const myDiv = document.querySelector('#globalmute');
+// // Get the existing div element
+// const myDiv = document.querySelector('#globalmute');
 
-// Initialize an array to keep track of the previously paused audio elements
-let pausedAudioElements = [];
+// // Initialize an array to keep track of the previously paused audio elements
+// let pausedAudioElements = [];
 
-// Add a click event listener to the existing div
-myDiv.addEventListener('click', () => {
-  // Get all audio elements on the page
-  const audioElements = document.querySelectorAll('audio');
+// // Add a click event listener to the existing div
+// myDiv.addEventListener('click', () => {
+//   // Get all audio elements on the page
+//   const audioElements = document.querySelectorAll('audio');
   
-  if (pausedAudioElements.length === 0) {
-    // Pause each audio element and add it to the array of paused audio elements
-    audioElements.forEach(audio => {
-      if (!audio.paused) {
-        audio.pause();
-        pausedAudioElements.push(audio);
-        console.log("Each active audio element has been paused and added to array.");
-      }
-      myDiv.style.opacity = .25;
-    });
-  } else {
-    // Play each previously paused audio element and clear the array of paused audio elements
-    pausedAudioElements.forEach(audio => audio.play());
-    pausedAudioElements = [];
-    console.log("Each paused audio element has been resumed and removed from array.");
-    myDiv.style.opacity = 1;
-  }
-});
+//   if (pausedAudioElements.length === 0) {
+//     // Pause each audio element and add it to the array of paused audio elements
+//     audioElements.forEach(audio => {
+//       if (!audio.paused) {
+//         audio.pause();
+//         pausedAudioElements.push(audio);
+//         console.log("Each active audio element has been paused and added to array.");
+//       }
+//       myDiv.style.opacity = .25;
+//     });
+//   } else {
+//     // Play each previously paused audio element and clear the array of paused audio elements
+//     pausedAudioElements.forEach(audio => audio.play());
+//     pausedAudioElements = [];
+//     console.log("Each paused audio element has been resumed and removed from array.");
+//     myDiv.style.opacity = 1;
+//   }
+// });
