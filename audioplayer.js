086c3803ab6d99cb2ceb572a72fb0b0918df0567
YoghurtJones <130816596+YoghurtJones@users.noreply.github.com>
2023-04-19 20:54:20 +1000

    D20

--- audioplayer.js
+++ audioplayer.js
@@ -1,8 +1,10 @@
-// Locally stop audio element and reset playback
+//
 
 var audioPlayElements = document.querySelectorAll(".audio-play");
-var audioElements = document.querySelectorAll(".audio-track");
 var volumeSliders = document.querySelectorAll(".volume-slider");
+var audioElements = document.querySelectorAll(".audio-track");
+var audioMuteElements = document.querySelectorAll(".audio-mute");
+var audioStopElements = document.querySelectorAll(".audio-stop");
 
 audioPlayElements.forEach(function (audioPlay, index) {
   audioPlay.addEventListener("click", function () {
@@ -29,11 +31,6 @@ volumeSliders.forEach(function (slider, index) {
   });
 });
 
-//Locally mute audio element.
-
-var audioMuteElements = document.querySelectorAll(".audio-mute");
-var audioElements = document.querySelectorAll(".audio-track");
-
 audioMuteElements.forEach(function (audioMute, index) {
   var audioElement = audioElements[index];
   var savedVolume = audioElement.volume; // Store the current volume
@@ -52,10 +49,6 @@ audioMuteElements.forEach(function (audioMute, index) {
   });
 });
 
-var audioStopElements = document.querySelectorAll(".audio-stop");
-var audioElements = document.querySelectorAll(".audio-track");
-var audioPlayElements = document.querySelectorAll(".audio-play");
-
 audioStopElements.forEach(function (audioStop, index) {
   audioStop.addEventListener("click", function () {
     var audioElement = audioElements[index];
@@ -68,63 +61,3 @@ audioStopElements.forEach(function (audioStop, index) {
     }
   });
 });
-
-// // Update boxshadow to reflect music type
-
-// var cardBoxShadow = document.querySelectorAll(".music");
-
-// cardBoxShadow.forEach(function(div) {
-//   div.addEventListener("click", function() {
-//     if (div.style.boxShadow) {
-//       div.style.boxShadow = "";
-//     } else {
-//       div.style.boxShadow = "0 0 20px red";
-//       console.log("Boxshadow style for music added to card.");
-//     }
-//   });
-// });
-
-// var cardBoxShadow = document.querySelectorAll(".ambience");
-
-// cardBoxShadow.forEach(function(div) {
-//   div.addEventListener("click", function() {
-//     if (div.style.boxShadow) {
-//       div.style.boxShadow = "";
-//     } else {
-//       div.style.boxShadow = "0 0 20px greenyellow";
-//       console.log("Boxshadow style for ambience added to card.");
-//     }
-//   });
-// });
-
-// // GLOBAL PAUSE
-
-// // Get the existing div element
-// const myDiv = document.querySelector('#globalmute');
-
-// // Initialize an array to keep track of the previously paused audio elements
-// let pausedAudioElements = [];
-
-// // Add a click event listener to the existing div
-// myDiv.addEventListener('click', () => {
-//   // Get all audio elements on the page
-//   const audioElements = document.querySelectorAll('audio');
-
-//   if (pausedAudioElements.length === 0) {
-//     // Pause each audio element and add it to the array of paused audio elements
-//     audioElements.forEach(audio => {
-//       if (!audio.paused) {
-//         audio.pause();
-//         pausedAudioElements.push(audio);
-//         console.log("Each active audio element has been paused and added to array.");
-//       }
-//       myDiv.style.opacity = .25;
-//     });
-//   } else {
-//     // Play each previously paused audio element and clear the array of paused audio elements
-//     pausedAudioElements.forEach(audio => audio.play());
-//     pausedAudioElements = [];
-//     console.log("Each paused audio element has been resumed and removed from array.");
-//     myDiv.style.opacity = 1;
-//   }
-// });
