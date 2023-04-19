4ba0e3445df00fbd4976b220d2f87ad95889ad93
YoghurtJones <130816596+YoghurtJones@users.noreply.github.com>
2023-04-19 19:03:30 +1000

    Audioplayer and swamp

--- audioplayer.js
+++ audioplayer.js
@@ -9,12 +9,14 @@ audioPlayElements.forEach(function (audioPlay, index) {
     var audioElement = audioElements[index];
     if (audioElement.paused) {
       audioElement.play();
+        console.log("Audio element is now playing.");
       audioPlay.classList.replace("fa-play", "fa-pause");
-      console.log("Audio element is now playing.");
+        console.log("Updated icon from play to pause.");
     } else {
       audioElement.pause();
+        console.log("Audio element is now paused.");
       audioPlay.classList.replace("fa-pause", "fa-play");
-      console.log("Audio element is now reset.");
+        console.log("Updated icon from pause to play.");
     }
   });
 });
@@ -23,8 +25,10 @@ volumeSliders.forEach(function (slider, index) {
   var audioElement = audioElements[index];
   slider.addEventListener("input", function () {
     audioElement.volume = slider.value;
+    console.log("Volume changed to: " + slider.value);
   });
 });
+
 
 //Locally mute audio element.
 
@@ -32,15 +36,19 @@ var audioMuteElements = document.querySelectorAll(".audio-mute");
 var audioElements = document.querySelectorAll(".audio-track");
 
 audioMuteElements.forEach(function (audioMute, index) {
+  var audioElement = audioElements[index];
+  var savedVolume = audioElement.volume; // Store the current volume
+  
   audioMute.addEventListener("click", function () {
-    var audioElement = audioElements[index];
     if (!audioElement.paused && audioElement.volume > 0) {
+      savedVolume = audioElement.volume; // Update savedVolume with current volume
       audioElement.volume = 0;
       audioMute.classList.replace("fa-volume-high", "fa-volume-xmark");
       console.log("Audio element volume set to 0.");
     } else {
+      audioElement.volume = savedVolume; // Set volume to savedVolume
       audioMute.classList.replace("fa-volume-xmark", "fa-volume-high");
-      console.log("Audio element volume set to 1.");
+      console.log("Audio element volume set to " + savedVolume + ".");
     }
   });
 });
