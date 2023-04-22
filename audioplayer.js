bf1217c82c29f2c61643a9e90b1ddbf4c550fa26
YoghurtJones <130816596+YoghurtJones@users.noreply.github.com>
2023-04-22 12:57:45 +1000

    Updated audiplayer.js
    There is now eventlisteners for touch displays.

--- audioplayer.js
+++ audioplayer.js
@@ -11,31 +11,78 @@ audioPlayElements.forEach(function (audioPlay, index) {
     var audioElement = audioElements[index];
     if (audioElement.paused) {
       audioElement.play();
-        console.log("Audio element is now playing.");
+      console.log("Audio element is now playing.");
       audioPlay.classList.replace("fa-play", "fa-pause");
-        console.log("Updated icon from play to pause.");
+      console.log("Updated icon from play to pause.");
     } else {
       audioElement.pause();
-        console.log("Audio element is now paused.");
+      console.log("Audio element is now paused.");
       audioPlay.classList.replace("fa-pause", "fa-play");
-        console.log("Updated icon from pause to play.");
+      console.log("Updated icon from pause to play.");
     }
   });
 });
 
+// volumeSliders.forEach(function (slider, index) {
+//   var audioElement = audioElements[index];
+//   slider.addEventListener("input", function () {
+//     audioElement.volume = slider.value;
+//     console.log("Volume changed to: " + slider.value);
+//   });
+// });
+
 volumeSliders.forEach(function (slider, index) {
   var audioElement = audioElements[index];
+
+  // Mouse events
   slider.addEventListener("input", function () {
     audioElement.volume = slider.value;
-    console.log("Volume changed to: " + slider.value);
+    console.log("INPUT Volume changed to: " + slider.value);
+  });
+
+  // Touch events
+  slider.addEventListener("touchmove", function () {
+    audioElement.volume = slider.value;
+    console.log("TOUCH Volume changed to: " + slider.value);
   });
 });
+
+// audioMuteElements.forEach(function (audioMute, index) {
+//   var audioElement = audioElements[index];
+//   var savedVolume = audioElement.volume; // Store the current volume
+
+//   audioMute.addEventListener("click", function () {
+//     if (!audioElement.paused && audioElement.volume > 0) {
+//       savedVolume = audioElement.volume; // Update savedVolume with current volume
+//       audioElement.volume = 0;
+//       audioMute.classList.replace("fa-volume-high", "fa-volume-xmark");
+//       console.log("Audio element volume set to 0.");
+//     } else {
+//       audioElement.volume = savedVolume; // Set volume to savedVolume
+//       audioMute.classList.replace("fa-volume-xmark", "fa-volume-high");
+//       console.log("Audio element volume set to " + savedVolume + ".");
+//     }
+//   });
+// });
 
 audioMuteElements.forEach(function (audioMute, index) {
   var audioElement = audioElements[index];
   var savedVolume = audioElement.volume; // Store the current volume
-  
+
   audioMute.addEventListener("click", function () {
+    if (!audioElement.paused && audioElement.volume > 0) {
+      savedVolume = audioElement.volume; // Update savedVolume with current volume
+      audioElement.volume = 0;
+      audioMute.classList.replace("fa-volume-high", "fa-volume-xmark");
+      console.log("Audio element volume set to 0.");
+    } else {
+      audioElement.volume = savedVolume; // Set volume to savedVolume
+      audioMute.classList.replace("fa-volume-xmark", "fa-volume-high");
+      console.log("Audio element volume set to " + savedVolume + ".");
+    }
+  });
+
+  audioMute.addEventListener("touchstart", function () {
     if (!audioElement.paused && audioElement.volume > 0) {
       savedVolume = audioElement.volume; // Update savedVolume with current volume
       audioElement.volume = 0;
@@ -49,9 +96,32 @@ audioMuteElements.forEach(function (audioMute, index) {
   });
 });
 
+// audioStopElements.forEach(function (audioStop, index) {
+//   audioStop.addEventListener("click", function () {
+//     var audioElement = audioElements[index];
+//     if (!audioElement.paused) {
+//       audioElement.pause();
+//       audioElement.currentTime = 0;
+//       audioPlayElements[index].classList.remove("fa-pause");
+//       audioPlayElements[index].classList.add("fa-play");
+//       console.log("Audio element has been stopped.");
+//     }
+//   });
+// });
+
 audioStopElements.forEach(function (audioStop, index) {
+  var audioElement = audioElements[index];
   audioStop.addEventListener("click", function () {
-    var audioElement = audioElements[index];
+    if (!audioElement.paused) {
+      audioElement.pause();
+      audioElement.currentTime = 0;
+      audioPlayElements[index].classList.remove("fa-pause");
+      audioPlayElements[index].classList.add("fa-play");
+      console.log("Audio element has been stopped.");
+    }
+  });
+
+  audioStop.addEventListener("touchstart", function () {
     if (!audioElement.paused) {
       audioElement.pause();
       audioElement.currentTime = 0;
