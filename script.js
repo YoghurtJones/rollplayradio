6a65c4387b7e759a2e28aaab031b3f58f4aabfd5
YoghurtJones <130816596+YoghurtJones@users.noreply.github.com>
2023-04-30 23:22:54 +1000

    Volume Slider Added Back.

--- script.js
+++ script.js
@@ -51,44 +51,44 @@ function controlAudioPlayer(audioPlayerID) {
     }
   });
 
-  volumeDown.addEventListener('click', function() {
-    if(audioTrack.volume >= 0.1 && audioTrack.volume <= 1) {
-      audioTrack.volume -= 0.1;
-    }else {
-      console.log('Volume is 0.');
-    }
-  })
+  // volumeDown.addEventListener('click', function() {
+  //   if(audioTrack.volume >= 0.1 && audioTrack.volume <= 1) {
+  //     audioTrack.volume -= 0.1;
+  //   }else {
+  //     console.log('Volume is 0.');
+  //   }
+  // })
 
-  volumeUp.addEventListener('click', function() {
-    if(audioTrack.volume >= 0 && audioTrack.volume < 1) { // use the correct condition
-      audioTrack.volume += 0.1; // use "+=" to decrease the volume
-    } else {
-      console.log('Volume is already at maximum.'); // update the console message
-    }
-  });
+  // volumeUp.addEventListener('click', function() {
+  //   if(audioTrack.volume >= 0 && audioTrack.volume < 1) { // use the correct condition
+  //     audioTrack.volume += 0.1; // use "+=" to decrease the volume
+  //   } else {
+  //     console.log('Volume is already at maximum.'); // update the console message
+  //   }
+  // });
   
 
-  // volumeSlider.addEventListener('input', function() {
-  //   audioTrack.volume = volumeSlider.value;
-  //   volumeLevel.value = volumeSlider.value;
-  //   console.log("Volume slider clicked.");
-  // });
+  volumeSlider.addEventListener('input', function() {
+    audioTrack.volume = volumeSlider.value;
+    volumeLevel.value = volumeSlider.value;
+    console.log("Volume slider clicked.");
+  });
 
-  // volumeSlider.addEventListener('change', function() {
-  //   audioTrack.volume = volumeSlider.value;
-  //   volumeLevel.value = volumeSlider.value;
-  //   console.log("Volume slider change received " + volumeSlider.value + ".");
-  // });
+  volumeSlider.addEventListener('change', function() {
+    audioTrack.volume = volumeSlider.value;
+    volumeLevel.value = volumeSlider.value;
+    console.log("Volume slider change received " + volumeSlider.value + ".");
+  });
 
   muteButton.addEventListener('click', function() {
     if (audioTrack.play && !audioTrack.muted) {
       muteButton.classList.remove('fa-volume-high');
-      muteButton.classList.add('fa-volume-off');
+      muteButton.classList.add('fa-volume-xmark');
       audioTrack.muted = true;
       console.log("Card audio muted.");
     } else {
       audioTrack.muted = false;
-      muteButton.classList.remove('fa-volume-off');
+      muteButton.classList.remove('fa-volume-xmark');
       muteButton.classList.add('fa-volume-high');
       console.log("Card audio unmuted.");
     }
@@ -117,11 +117,11 @@ function controlAudioPlayer(audioPlayerID) {
 // AMBIENCE
 controlAudioPlayer('forest');
 
-// // MUSIC
-// controlAudioPlayer('magic-item-shop');
+// MUSIC
+controlAudioPlayer('magic-item-shop');
 
-// // ACTION
-// controlAudioPlayer('search');
+// ACTION
+controlAudioPlayer('search');
 
-// // ENEMY
-// controlAudioPlayer('necromancer');
+// ENEMY
+controlAudioPlayer('necromancer');
