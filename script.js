25aec0732836cafb55251bbd6af73658190e1f29
YoghurtJones <130816596+YoghurtJones@users.noreply.github.com>
2023-04-30 19:44:59 +1000

    Updated media query (iPhone)

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-// fade div after scrolling down 150 pixels
-window.addEventListener('scroll', function() {
-  const myDiv = document.getElementById('alert');
-  if (window.scrollY > 300) {
-    myDiv.style.display = 'none';
-    myDiv.classList.add('hidden');
-  } else {
-    myDiv.classList.remove('hidden');
-  }
-});
+// // fade div after scrolling down 150 pixels
+// window.addEventListener('scroll', function() {
+//   const myDiv = document.getElementById('alert');
+//   if (window.scrollY > 150) {
+//     myDiv.style.display = 'none';
+//     myDiv.classList.add('hidden');
+//   } else {
+//     myDiv.classList.remove('hidden');
+//   }
+// });
 
 
 // Function to control an audio player
